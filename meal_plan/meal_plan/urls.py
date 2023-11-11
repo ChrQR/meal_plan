44e64e443e27bde658213c9b7321d67e5f1f58fd
ChrQR <christian@rannes.com>
2023-11-11 14:29:02 +0100

    updated settings.py with weekle_plan app created paths and view functions for home and weekly.

--- meal_plan/meal_plan/urls.py
+++ meal_plan/meal_plan/urls.py
@@ -16,7 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from weekly_plan import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', views.home, name="home"),
+    path('weekly_plan', views.weekly_plan, name="weekly_plan"),
 ]
--- meal_plan/weekly_plan/views.py
+++ meal_plan/weekly_plan/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+
+def home(request):
+    return render(request, 'weekly_plan/home.html')
+
+def weekly_plan(request):
+    return render(request, 'weekly_plan/weekly_plan.html')
