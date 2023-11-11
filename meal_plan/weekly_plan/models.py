d6675db1250501e5d57ea971ad5e45ee10f3aa60
ChrQR <christian@rannes.com>
2023-11-11 15:06:39 +0100

    created initial models and migrated them

--- meal_plan/weekly_plan/models.py
+++ meal_plan/weekly_plan/models.py
@@ -1,3 +1,21 @@
 from django.db import models
+import random
 
 # Create your models here.
+class Meal(models.Model):
+    name = models.CharField(max_length=50)
+
+class Weekly(models.Model):
+
+    def populateWeek():
+        try:
+            meal_list = []
+            while(len(meal_list) < 8):
+                random_meal_id = random.randint(0, Meal.objects.count() - 1)
+                random_meal = Meal.objects.all()[random_meal_id]
+                meal_list.append(random_meal)
+            return meal_list
+        except:
+            return False
+    
+    weekly_plan = populateWeek()
